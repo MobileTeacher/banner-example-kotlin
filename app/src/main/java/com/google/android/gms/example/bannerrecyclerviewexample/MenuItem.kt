1915bb4a69173af9d0c25adabf4fdeee95ee3a90
Hallison Oliveira da Paz <hallison.paz@prof.infnet.edu.br>
2019-09-12 19:48:13 -0300

    refactored model to kotlin

---
+++ app/src/main/java/com/google/android/gms/example/bannerrecyclerviewexample/MenuItem.kt
@@ -0,0 +1,9 @@
+package com.google.android.gms.example.bannerrecyclerviewexample
+
+class MenuItem(
+        val name: String,
+        val description: String,
+        val price: String,
+        val category: String,
+        val imageName: String
+)
